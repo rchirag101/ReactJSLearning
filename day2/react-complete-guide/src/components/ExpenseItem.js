ffa3302ca36288c72c39920ee0bea8c27443d9ee
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-19 18:22:17 +0530

    Added Basic CSS Styling

--- day2/react-complete-guide/src/components/ExpenseItem.js
+++ day2/react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem(){
-    return <h2>Expense Item component</h2>;
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+    return (
+        <div className="expense-item">
+            <div>19/10/2021</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$400</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
