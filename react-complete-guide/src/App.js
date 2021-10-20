24a20735f6a491bbd8085ee997c484926776a334
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-20 15:28:18 +0530

    passed data from App.js to ExpenseItem.js using props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,7 +4,7 @@ function App() {
     const expenses = [
         {
             id: "e1",
-            title: "Toilet Paper",
+            title: "Laptop",
             amount: 94.12,
             date: new Date(2020, 7, 14),
         },
