1e2f2925124246a538e740eaa7a66c04e0485b54
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-20 16:20:48 +0530

    added styling to ExpenseDate

--- react-complete-guide/src/components/parts/ExpenseDate.js
+++ react-complete-guide/src/components/parts/ExpenseDate.js
@@ -1,13 +1,15 @@
+import "./ExpenseDate.css";
+
 function ExpenseDate(props) {
     const day = props.date.toLocaleString("en-US", { day: "2-digit" });
     const month = props.date.toLocaleString("en-US", { month: "long" });
     const year = props.date.getFullYear();
 
     return (
-        <div>
-            <div>{day}</div>
-            <div>{month}</div>
-            <div>{year}</div>
+        <div className="expense-date">
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
         </div>
     );
 }
