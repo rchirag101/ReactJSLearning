6a107d44c6ff480ac5bf9861fba891a03bc67a07
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-22 18:20:10 +0530

    change title using useState()
    started with state management in React

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,22 +1,28 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./css/ExpenseItem.css";
 import Card from "./Card";
 
 function ExpenseItem(props) {
+    // useState() returns array of two values
+    // the first element is the current state value, here is title
+    // and And the second element is a function for updating that, here setTitle
+    const [title, setTitle] = useState(props.title);
 
-
-    function clickHandler(){
-
-        console.log("Button clicked!!!");
+    function clickHandler() {
+        setTitle("New Title");
+        console.log(title);
     }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button button onClick={clickHandler}>Change Title</button>
+            <button button onClick={clickHandler}>
+                Change Title
+            </button>
         </Card>
     );
 }
