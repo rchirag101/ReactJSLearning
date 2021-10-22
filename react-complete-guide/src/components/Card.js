b743fa8ec87419edddbcba833abebee8923cf221
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-22 18:19:03 +0530

    imported React in header

--- react-complete-guide/src/components/Card.js
+++ react-complete-guide/src/components/Card.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./css/Card.css";
 
 const Card = (props) => {
--- react-complete-guide/src/components/ExpenseDate.js
+++ react-complete-guide/src/components/ExpenseDate.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./css/ExpenseDate.css";
 
 function ExpenseDate(props) {
