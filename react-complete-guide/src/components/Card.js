68ed8e6dd552a81056d68ff362b7d1ded91d3e7b
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-22 17:08:16 +0530

    used arrow function

--- react-complete-guide/src/components/Card.js
+++ react-complete-guide/src/components/Card.js
@@ -1,6 +1,7 @@
 import "./css/Card.css";
 
-const Card = (props) => { // const Card = (props) => {} and function Card(props){} are same
+const Card = (props) => {
+    // const Card = (props) => {} and function Card(props){} are same
 
     const classes = "card " + props.className;
     return <div className={classes}>{props.children}</div>;
