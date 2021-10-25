da1ec6c7eb70664a413eb9509ac7066b3a1b5fc1
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-25 12:10:50 +0530

    ExpenseForm added in NewExpense

--- react-complete-guide/src/components/NewExpense.js
+++ react-complete-guide/src/components/NewExpense.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./css/NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
-function NewExpense() {}
+function NewExpense() {
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    );
+}
 
 export default NewExpense;
