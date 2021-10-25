3c84a6814d405046b49d110b9f026d4cf20390a9
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-25 12:54:13 +0530

    added multiple states in ExpenseForm

--- react-complete-guide/src/components/ExpenseForm.js
+++ react-complete-guide/src/components/ExpenseForm.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/ExpenseForm.css";
 
 function ExpenseForm() {
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+
     function titleChangeHandler(event) {
-        console.log(event.target.value);
+        // console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+    }
+
+    function amountChangeHandler(event) {
+        // console.log(event.target.value);
+        setEnteredAmount(event.target.value);
+    }
+
+    function dateChangeHandler(event) {
+        // console.log(event.target.value);
+        setEnteredDate(event.target.value);
     }
 
     return (
@@ -15,11 +30,21 @@ function ExpenseForm() {
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input
+                        type="number"
+                        min="0.01"
+                        step="0.01"
+                        onChange={amountChangeHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2020-01-01" max="2022-12-31" />
+                    <input
+                        type="date"
+                        min="2020-01-01"
+                        max="2022-12-31"
+                        onChange={dateChangeHandler}
+                    />
                 </div>
             </div>
             <div className="new-expense__actions">
