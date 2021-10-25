946e1687bab7b3babfdc5dd5c659151a79997991
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-25 18:58:17 +0530

    tried to make Add Expense dynamic

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,39 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense";
 
+const DUMMY_EXPENSES = [
+    {
+        id: "e1",
+        title: "Laptop",
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    {
+        id: "e2",
+        title: "New TV",
+        amount: 799.49,
+        date: new Date(2021, 2, 12),
+    },
+    {
+        id: "e3",
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: "e4",
+        title: "New Desk (Wooden)",
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
+
 function App() {
-    const expenses = [
-        {
-            id: "e1",
-            title: "Laptop",
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        {
-            id: "e2",
-            title: "New TV",
-            amount: 799.49,
-            date: new Date(2021, 2, 12),
-        },
-        {
-            id: "e3",
-            title: "Car Insurance",
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: "e4",
-            title: "New Desk (Wooden)",
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     function addExpenseHandler(expense) {
-        console.log("In App.js");
+        // console.log("In App.js");
         console.log(expense);
+        console.log(expenses);
+        setExpenses((prevExpenses) => {
+            return [...prevExpenses, expense];
+        });
     }
+
     return (
         <div>
             <center>
