d37650bb294b5d40a736e14b9ab0fed59061980e
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-25 11:58:17 +0530

    started Adding Form Inputs

---
+++ react-complete-guide/src/components/ExpenseForm.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./css/ExpenseForm.css";
+
+function ExpenseForm() {}
+
+export default ExpenseForm;
---
+++ react-complete-guide/src/components/NewExpense.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./css/NewExpense.css";
+
+function NewExpense() {}
+
+export default NewExpense;
