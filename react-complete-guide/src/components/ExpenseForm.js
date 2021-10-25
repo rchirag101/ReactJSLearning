28f791930f4474c748bc81e057e105bd0a627545
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-25 13:07:27 +0530

    used One State Instead of multiple

--- react-complete-guide/src/components/ExpenseForm.js
+++ react-complete-guide/src/components/ExpenseForm.js
@@ -2,23 +2,41 @@ import React, { useState } from "react";
 import "./css/ExpenseForm.css";
 
 function ExpenseForm() {
-    const [enteredTitle, setEnteredTitle] = useState("");
-    const [enteredAmount, setEnteredAmount] = useState("");
-    const [enteredDate, setEnteredDate] = useState("");
+    // const [enteredTitle, setEnteredTitle] = useState("");
+    // const [enteredAmount, setEnteredAmount] = useState("");
+    // const [enteredDate, setEnteredDate] = useState("");
+
+    const [userInput, setUserInput] = useState({
+        enteredTitle: "",
+        enteredAmount: "",
+        enteredDate: "",
+    });
 
     function titleChangeHandler(event) {
         // console.log(event.target.value);
-        setEnteredTitle(event.target.value);
+        // setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value,
+        });
     }
 
     function amountChangeHandler(event) {
         // console.log(event.target.value);
-        setEnteredAmount(event.target.value);
+        // setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        });
     }
 
     function dateChangeHandler(event) {
         // console.log(event.target.value);
-        setEnteredDate(event.target.value);
+        // setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        });
     }
 
     return (
