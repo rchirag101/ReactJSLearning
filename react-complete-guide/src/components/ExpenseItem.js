fc1e02b10fa3652d2607553f4f7e45c62cd67113
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-26 12:05:00 +0530

    useState removed

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+// import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./css/ExpenseItem.css";
 import Card from "./Card";
