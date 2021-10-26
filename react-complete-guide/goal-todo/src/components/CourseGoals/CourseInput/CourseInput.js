8b8b9a3331ac5c91dd15dea1bb2dcbe24ff1595e
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-26 18:36:43 +0530

    simple validation handeled

--- react-complete-guide/goal-todo/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-complete-guide/goal-todo/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+    const [enteredValue, setEnteredValue] = useState("");
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    const [isValid, setIsValid] = useState(true);
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    const goalInputChangeHandler = (event) => {
+        setEnteredValue(event.target.value);
+    };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        if (enteredValue.trim().length === 0) {
+            return;
+        }
+
+        props.onAddGoal(enteredValue);
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label>Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler} />
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default CourseInput;
