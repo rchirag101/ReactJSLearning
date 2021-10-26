5615e1cf46b3a066ad316fd38dbf0eaad788bff8
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-26 12:57:59 +0530

    wrapped with <li>

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -16,16 +16,18 @@ function ExpenseItem(props) {
     // }
 
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            {/* <button button onClick={clickHandler}>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+                {/* <button button onClick={clickHandler}>
                 Change Title
             </button> */}
-        </Card>
+            </Card>
+        </li>
     );
 }
 
