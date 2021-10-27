7fccc47c44261cccec7c4b2771d2940ae943aa81
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-27 18:34:45 +0530

    logic to get new user's data

--- react-complete-guide/add-user/src/App.js
+++ react-complete-guide/add-user/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
+    const [usersList, setUsersList] = useState([]);
+
+    function addUserHandler(uName, uAge) {
+        setUsersList((prevUsersList) => {
+            return [...prevUsersList, { name: uName, age: uAge }];
+        });
+    }
+
     return (
         <div>
-            <AddUser />
-            <UsersList users={[]} />
+            <AddUser onAddUser={addUserHandler} />
+            <UsersList users={usersList} />
         </div>
     );
 }
