5870e429c46dea35494eead9562f4d762f6a156a
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-27 11:24:18 +0530

    formatted Button.js

--- react-complete-guide/goal-todo/src/components/UI/Button/Button.js
+++ react-complete-guide/goal-todo/src/components/UI/Button/Button.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css';
+import "./Button.css";
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+const Button = (props) => {
+    return (
+        <button type={props.type} className="button" onClick={props.onClick}>
+            {props.children}
+        </button>
+    );
 };
 
 export default Button;
