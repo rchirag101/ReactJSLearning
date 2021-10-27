fe660a10791d4117205042eecd7b6d5ec57b6e8c
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-27 13:02:15 +0530

    applied CSS Module's style to form-control

--- react-complete-guide/goal-todo/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-complete-guide/goal-todo/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -54,10 +54,14 @@ const CourseInput = (props) => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <FormControl invalid={!isValid}>
+            <div
+                className={`${styles["form-control"]} ${
+                    !isValid && styles.invalid
+                }`}
+            >
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler} />
-            </FormControl>
+            </div>
 
             <Button type="submit">Add Goal</Button>
         </form>
