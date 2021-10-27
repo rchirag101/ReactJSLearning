4debe3e7b617b7354467770470b1dc434e73f213
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-27 12:43:00 +0530

    applied style to button using dynamic object

--- react-complete-guide/goal-todo/src/components/UI/Button/Button.js
+++ react-complete-guide/goal-todo/src/components/UI/Button/Button.js
@@ -31,7 +31,11 @@ import styles from "./Button.module.css";
 
 const Button = (props) => {
     return (
-        <button type={props.type} className="button" onClick={props.onClick}>
+        <button
+            type={props.type}
+            className={styles.button}
+            onClick={props.onClick}
+        >
             {props.children}
         </button>
     );
