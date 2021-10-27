7b96a6177c59723099c2ca614cd0e7ea4c653712
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-27 13:20:15 +0530

    moved css files to css folder

--- react-complete-guide/goal-todo/src/App.js
+++ react-complete-guide/goal-todo/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-import './App.css';
+import './css/App.css';
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
