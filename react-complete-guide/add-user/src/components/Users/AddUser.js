e4afa404a8315e65c88e700658bfd28c36ea7982
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-27 17:28:03 +0530

    added User Input State

--- react-complete-guide/add-user/src/components/Users/AddUser.js
+++ react-complete-guide/add-user/src/components/Users/AddUser.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 function AddUser(props) {
+    // Below is a JavaScript syntax, which is called array de-structuring and we use it, because useState always returns an array with exactly two elements. And with this syntax, we're pulling these elements out of that returned array. And we store them in separate constants, of the name enteredUsername and setEnteredUsername. And I chose those names for those constants, because the first element of that returned array, is the current state snapshot. And every time this Component re-renders, which it, for example, does when the state is updated, this concept will hold the latest state snapshot and setEnteredUsername on the other hand, holds a function which we can call, to change that state and to then trigger such a re-render cycle.
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
+
+    function usernameChangeHandler(event) {
+        setEnteredUsername(event.target.value);
+    }
+    function ageChangeHandler(event) {
+        setEnteredAge(event.target.value);
+    }
+
     function addUserHandler(event) {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge);
     }
 
     return (
@@ -13,9 +25,13 @@ function AddUser(props) {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input
+                    id="username"
+                    type="text"
+                    onChange={usernameChangeHandler}
+                />
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" onChange={ageChangeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
