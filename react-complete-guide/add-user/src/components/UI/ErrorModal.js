049c7ff74823729db9d25e7eccab882184638cae
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-27 19:02:03 +0530

    added ErrorModal

--- react-complete-guide/add-user/src/components/UI/ErrorModal.js
+++ react-complete-guide/add-user/src/components/UI/ErrorModal.js
@@ -1,6 +1,25 @@
 import React from "react";
+import Button from "./Button";
+import Card from "./Card";
 import classes from "./ErrorModal.module.css";
 
-function ErrorModal() {}
+function ErrorModal(props) {
+    return (
+        <div>
+            <div className={classes.backdrop}></div>
+            <Card className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={classes.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={classes.actions}>
+                    <Button>Okay</Button>
+                </footer>
+            </Card>
+        </div>
+    );
+}
 
 export default ErrorModal;
--- react-complete-guide/add-user/src/components/Users/AddUser.js
+++ react-complete-guide/add-user/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
 function AddUser(props) {
@@ -38,26 +39,32 @@ function AddUser(props) {
     }
 
     return (
-        // we can use className for Card because it defined in Card.js as props.className
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    onChange={usernameChangeHandler}
-                    value={enteredUsername}
-                />
-                <label htmlFor="age">Age (Years)</label>
-                <input
-                    id="age"
-                    type="number"
-                    onChange={ageChangeHandler}
-                    value={enteredAge}
-                />
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            <ErrorModal
+                title="An Error occurred!!"
+                message="Something went wrong!!"
+            />
+            {/* we can use className for Card because it defined in Card.js as props.className */}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                        onChange={usernameChangeHandler}
+                        value={enteredUsername}
+                    />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input
+                        id="age"
+                        type="number"
+                        onChange={ageChangeHandler}
+                        value={enteredAge}
+                    />
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
     );
 }
 export default AddUser;
