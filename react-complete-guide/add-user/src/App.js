87a24bea88037d399afda8ce93f4bf02231415f8
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-28 12:14:29 +0530

    Done Section 8: add-user practice project
    endNote Added in App.js

--- react-complete-guide/add-user/src/App.js
+++ react-complete-guide/add-user/src/App.js
@@ -23,3 +23,7 @@ function App() {
 }
 
 export default App;
+
+// endNote : we used all those core features and building blocks that are important for React. Components, props, state with the use State Hook, lifting state up and all of these things. 
+// We used styling. We passed functions between the different components. 
+// So all these core patterns and concepts which you basically need for every React application you're going to build, we have them all in here. And that's why this hopefully, was a great practice project
