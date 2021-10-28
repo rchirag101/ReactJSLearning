87fec6d36a3f2da1e8bfb352e3fc41ad8fa860be
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-28 16:36:12 +0530

    integrated react-bootstrap
    added basic form

--- tasks/user-management/user-management/src/App.js
+++ tasks/user-management/user-management/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Wrapper from "./components/Helpers/Wrapper";
+
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
 	return (
 		<Wrapper>
-			<h1>Hello</h1>
+			<form>
+				<label htmlFor="email">Email</label>
+				<input id="email" type="email" />
+				<label htmlFor="password">Password</label>
+				<input id="password" type="password" />
+				<Button type="submit">Login</Button>
+			</form>
 		</Wrapper>
 	);
 }
--- tasks/user-management/user-management/src/index.js
+++ tasks/user-management/user-management/src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import $ from "jquery";
-import Popper from "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import "./css/index.css";
 import App from "./App";
 
