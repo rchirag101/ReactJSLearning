f1071b42b185ae9f1185d9112d6095b1f4099a9c
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-28 15:44:08 +0530

    task project initialized : user-management
    bootstrap installed

---
+++ tasks/user-management/user-management/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Wrapper from "./components/Helpers/Wrapper";
+function App() {
+	return (
+		<Wrapper>
+			<h1>Hello</h1>
+		</Wrapper>
+	);
+}
+
+export default App;
---
+++ tasks/user-management/user-management/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import "./css/index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
