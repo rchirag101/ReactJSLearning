641dd6985c860f8b63b1b267f80ac74f48e54a4d
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-28 13:09:36 +0530

    start of Section 9
    Wrapper comoponent added add-user practise project

--- react-complete-guide/add-user/src/components/Users/AddUser.js
+++ react-complete-guide/add-user/src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
@@ -54,7 +55,7 @@ function AddUser(props) {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && (
                 <ErrorModal
                     title={error.title}
@@ -82,7 +83,7 @@ function AddUser(props) {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 }
 export default AddUser;
