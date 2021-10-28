2682e8473061c26d86f70fd447022a1f23b0dbae
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-28 11:13:52 +0530

    added ErrorModal on specific error

--- react-complete-guide/add-user/src/components/Users/AddUser.js
+++ react-complete-guide/add-user/src/components/Users/AddUser.js
@@ -8,6 +8,7 @@ function AddUser(props) {
     // Below is a JavaScript syntax, which is called array de-structuring and we use it, because useState always returns an array with exactly two elements. And with this syntax, we're pulling these elements out of that returned array. And we store them in separate constants, of the name enteredUsername and setEnteredUsername. And I chose those names for those constants, because the first element of that returned array, is the current state snapshot. And every time this Component re-renders, which it, for example, does when the state is updated, this concept will hold the latest state snapshot and setEnteredUsername on the other hand, holds a function which we can call, to change that state and to then trigger such a re-render cycle.
     const [enteredUsername, setEnteredUsername] = useState("");
     const [enteredAge, setEnteredAge] = useState("");
+    const [error, setError] = useState();
 
     function usernameChangeHandler(event) {
         setEnteredUsername(event.target.value);
@@ -23,10 +24,20 @@ function AddUser(props) {
             enteredUsername.trim().length === 0 ||
             enteredAge.trim().length === 0
         ) {
+            setError({
+                title: "Invalid Input",
+                message:
+                    "Please enter a valid name and age (non-empty values).",
+            });
             return;
         }
         if (+enteredAge < 1) {
             //So we have a string here, enteredAge, a string not a number. Now I'm comparing it to a number here. And generally that should work in JavaScript but to be super safe we can force a conversion of enteredAge to a number by adding a PLUS here. And with that we ensure that well, it is a number.
+
+            setError({
+                title: "Invalid Age",
+                message: "Please enter a valid age (>0).",
+            });
             return;
         }
 
@@ -40,10 +51,9 @@ function AddUser(props) {
 
     return (
         <div>
-            <ErrorModal
-                title="An Error occurred!!"
-                message="Something went wrong!!"
-            />
+            {error && (
+                <ErrorModal title={error.title} message={error.message} />
+            )}
             {/* we can use className for Card because it defined in Card.js as props.className */}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
