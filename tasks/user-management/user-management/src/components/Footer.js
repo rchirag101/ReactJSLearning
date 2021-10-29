ad7aeb416b3fff71d28f9f72f9d32b47b6a7d809
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-29 18:21:13 +0530

    changed Footer styling

--- tasks/user-management/user-management/src/components/Footer.js
+++ tasks/user-management/user-management/src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import classes from "./css/Footer.module.css";
 
 function Footer() {
 	return (
-		<footer className="footer mt-5 py-3 bg-dark">
+		<footer className={`footer mt-5 py-3 bg-dark ${classes.myFooter}`}>
 			<Container>
 				<p className="m-0 text-center text-white">
 					Made with ♥ by Chirag
