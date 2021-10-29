e21dfb9411f17513cac059ccaebe16caaaba132a
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-29 12:20:34 +0530

    split Backdrop and ModalOverlay

--- react-complete-guide/add-user/src/components/UI/ErrorModal.js
+++ react-complete-guide/add-user/src/components/UI/ErrorModal.js
@@ -3,23 +3,27 @@ import Button from "./Button";
 import Card from "./Card";
 import classes from "./css/ErrorModal.module.css";
 
+function Backdrop(props) {
+	return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+}
+function ModalOverlay(props) {
+	return (
+		<Card className={classes.modal}>
+			<header className={classes.header}>
+				<h2>{props.title}</h2>
+			</header>
+			<div className={classes.content}>
+				<p>{props.message}</p>
+			</div>
+			<footer className={classes.actions}>
+				<Button onClick={props.onConfirm}>Okay</Button>
+			</footer>
+		</Card>
+	);
+}
+
 function ErrorModal(props) {
-    return (
-        <React.Fragment>
-            <div className={classes.backdrop} onClick={props.onConfirm}></div>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
-        </React.Fragment>
-    );
+	return <React.Fragment></React.Fragment>;
 }
 
 export default ErrorModal;
