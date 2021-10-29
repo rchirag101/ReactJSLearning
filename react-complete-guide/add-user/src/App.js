ac1482b8e7a433b12c1fa85c2ed9547edfb04ee3
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-29 11:29:15 +0530

    used React Fragment

--- react-complete-guide/add-user/src/App.js
+++ react-complete-guide/add-user/src/App.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
-    const [usersList, setUsersList] = useState([]);
+	const [usersList, setUsersList] = useState([]);
 
-    function addUserHandler(uName, uAge) {
-        setUsersList((prevUsersList) => {
-            return [
-                { id: Math.random().toString(), name: uName, age: uAge },
-                ...prevUsersList,
-            ];
-        });
-    }
+	function addUserHandler(uName, uAge) {
+		setUsersList((prevUsersList) => {
+			return [
+				{ id: Math.random().toString(), name: uName, age: uAge },
+				...prevUsersList,
+			];
+		});
+	}
 
-    return (
-        <React.Fragment>
-            <AddUser onAddUser={addUserHandler} />
-            <UsersList users={usersList} />
-        </React.Fragment>
-    );
+	return (
+		<Fragment>
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={usersList} />
+		</Fragment>
+	);
 }
 
 export default App;
 
-// endNote : we used all those core features and building blocks that are important for React. Components, props, state with the use State Hook, lifting state up and all of these things. 
-// We used styling. We passed functions between the different components. 
+// endNote : we used all those core features and building blocks that are important for React. Components, props, state with the use State Hook, lifting state up and all of these things.
+// We used styling. We passed functions between the different components.
 // So all these core patterns and concepts which you basically need for every React application you're going to build, we have them all in here. And that's why this hopefully, was a great practice project
