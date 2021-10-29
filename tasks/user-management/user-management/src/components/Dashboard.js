bc46f10fe3d6b0ce4b8e2dfc8ff3c9ff9fa8d0fc
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-10-29 16:30:02 +0530

    tried to add react-data-table-component

--- tasks/user-management/user-management/src/components/Dashboard.js
+++ tasks/user-management/user-management/src/components/Dashboard.js
@@ -1,18 +1,74 @@
 import React from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import classes from "./css/Dashboard.module.css";
+
 import Wrapper from "./Helpers/Wrapper";
+import DataTable from "react-data-table-component";
 
 function Dashboard(props) {
+	const columns = [
+		{
+			name: "First Name",
+			selector: (row) => row.fn,
+		},
+		{
+			name: "Last Name",
+			selector: (row) => row.ln,
+		},
+		{
+			name: "Phone Number",
+			selector: (row) => row.phone,
+		},
+		{
+			name: "Email",
+			selector: (row) => row.email,
+		},
+		{
+			name: "Password",
+			selector: (row) => row.password,
+		},
+		{
+			name: "Actions",
+			selector: (row) => row.actions,
+		},
+	];
+
+	const data = [
+		{
+			id: 1,
+			fn: "Chirag",
+			ln: "Ramchandanee",
+			phone: "9898989898",
+			email: "cr@gmail.com",
+			password: "1234",
+			actions: "Edit | Delete",
+		},
+		{
+			id: 2,
+			fn: "Chirag",
+			ln: "Ramchandanee",
+			phone: "9898989898",
+			email: "cr@gmail.com",
+			password: "1234",
+			actions: "Edit | Delete",
+		},
+	];
 	return (
 		<Wrapper>
-            <h2>Welcome</h2>
-			<ul>
-				{["Alex", "John", "Jaz", "fedrik", "missali"].map(
-					(user, idx) => {
-						return <li key={idx}>{user}</li>;
-					}
-				)}
-			</ul>
+			<Container className={`mt-4 ${classes.myContainer}`}>
+				{/* <h2>Welcome</h2>
+				<ul>
+					{["Alex", "John", "Jaz", "fedrik", "missali"].map(
+						(user, idx) => {
+							return <li key={idx}>{user}</li>;
+						}
+					)}
+				</ul> */}
+
+				<DataTable columns={columns} data={data}></DataTable>
+			</Container>
 		</Wrapper>
 	);
 }
