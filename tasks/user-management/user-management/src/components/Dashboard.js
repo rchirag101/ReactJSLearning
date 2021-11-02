315a7646ea76405bdc31d72c3200744bc87c74b8
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-02 17:12:31 +0530

    displayed dynamic data

--- tasks/user-management/user-management/src/components/Dashboard.js
+++ tasks/user-management/user-management/src/components/Dashboard.js
@@ -7,24 +7,7 @@ import classes from "./css/Dashboard.module.css";
 import Wrapper from "./Helpers/Wrapper";
 
 function Dashboard(props) {
-	const users = [
-		{
-			id: 1,
-			fn: "Chirag",
-			ln: "Ramchandanee",
-			phone: "9898989898",
-			email: "cr@gmail.com",
-			password: "1234",
-		},
-		{
-			id: 2,
-			fn: "Pavan",
-			ln: "Ramchandanee",
-			phone: "7878787878",
-			email: "pr@gmail.com",
-			password: "asdf ",
-		},
-	];
+	var users = JSON.parse(localStorage.getItem("users"));
 
 	return (
 		<Wrapper>
@@ -44,32 +27,38 @@ function Dashboard(props) {
 						</tr>
 					</thead>
 					<tbody className="align-middle">
-						{users.map((item) => (
-							<tr key={item.id}>
-								<td>{item.id}</td>
-								<td>{item.fn}</td>
-								<td>{item.ln}</td>
-								<td>{item.phone}</td>
-								<td>{item.email}</td>
-								<td>{item.password}</td>
-								<td>
-									<Button
-										variant="primary"
-										href="/editUserData"
-										className="m-1"
-									>
-										Edit
-									</Button>
-									<Button
-										variant="danger"
-										href="/"
-										className="m-1"
-									>
-										Delete
-									</Button>
-								</td>
+						{users === null ? (
+							<tr>
+								<h2>No Users Found</h2>
 							</tr>
-						))}
+						) : (
+							users.map((item) => (
+								<tr key={item.id}>
+									<td>{item.id}</td>
+									<td>{item.firstname}</td>
+									<td>{item.lastname}</td>
+									<td>{item.phone}</td>
+									<td>{item.email}</td>
+									<td>{item.password}</td>
+									<td>
+										<Button
+											variant="primary"
+											href="/editUserData"
+											className="m-1"
+										>
+											Edit
+										</Button>
+										<Button
+											variant="danger"
+											href="/"
+											className="m-1"
+										>
+											Delete
+										</Button>
+									</td>
+								</tr>
+							))
+						)}
 					</tbody>
 				</Table>
 			</Container>
