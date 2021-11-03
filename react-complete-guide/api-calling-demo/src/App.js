7e313461f35e3db746c61f470561db421a1456f0
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-03 12:46:09 +0530

    converted App to class component

--- react-complete-guide/api-calling-demo/src/App.js
+++ react-complete-guide/api-calling-demo/src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
 import "./css/App.css";
 
-function App() {
-	return (
-		<center>
-			<h1>API Calling Demo</h1>
-		</center>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<center>
+				<h1>API Calling Demo</h1>
+			</center>
+		);
+	}
 }
 
 export default App;
