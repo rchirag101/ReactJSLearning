fd2451882f3d6e1b0583094f7640f662affe1209
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-03 13:23:43 +0530

    API calling done using reqres.in

--- react-complete-guide/api-calling-demo/src/App.js
+++ react-complete-guide/api-calling-demo/src/App.js
@@ -2,10 +2,61 @@ import React from "react";
 import "./css/App.css";
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			users: null,
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://reqres.in/api/users").then((response) => {
+			response.json().then((result) => {
+				console.log(result.data);
+				this.setState({ users: result.data });
+			});
+		});
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<h1>API Calling Demo</h1>
+				<center>
+					<table>
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Avatar</th>
+								<th>First Name</th>
+								<th>Last Name</th>
+								<th>Email</th>
+							</tr>
+						</thead>
+						<tbody>
+							{this.state.users
+								? this.state.users.map((item, i) => {
+										return (
+											<tr key={i}>
+												<td>{i}</td>
+												<td>
+													<img
+														src={item.avatar}
+														alt="Avatar"
+														height="75px"
+														width="75px"
+													/>
+												</td>
+												<td>{item.first_name}</td>
+												<td>{item.last_name}</td>
+												<td>{item.email}</td>
+											</tr>
+										);
+								  })
+								: null}
+						</tbody>
+					</table>
+				</center>
 			</div>
 		);
 	}
