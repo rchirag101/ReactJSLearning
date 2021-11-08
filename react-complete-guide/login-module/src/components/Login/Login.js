6e948f891d086977a33ba00d497e6c02df5e784b
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-08 16:58:49 +0530

    Done Section 10: Advanced: Handling Side Effects, Using Reducers & Using the Context API
    used Forward Refs (used in rarely)

--- react-complete-guide/login-module/src/components/Login/Login.js
+++ react-complete-guide/login-module/src/components/Login/Login.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
-
-import AuthContext from "../../context/auth-context";
+import React, {
+	useState,
+	useEffect,
+	useReducer,
+	useContext,
+	useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../context/auth-context";
 import Input from "../UI/Input/Input";
-
 import classes from "./Login.module.css";
 
 const emailReducer = (state, action) => {
@@ -45,6 +49,9 @@ const Login = (props) => {
 	});
 
 	const authCtx = useContext(AuthContext);
+
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
 
 	useEffect(() => {
 		console.log("EFFECT RUNNING");
@@ -93,13 +100,20 @@ const Login = (props) => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		authCtx.onLogin(emailState.value, passwordState.value);
+		if (formIsValid) {
+			authCtx.onLogin(emailState.value, passwordState.value);
+		} else if (!emailIsValid) {
+			emailInputRef.current.focus();
+		} else {
+			passwordInputRef.current.focus();
+		}
 	};
 
 	return (
 		<Card className={classes.login}>
 			<form onSubmit={submitHandler}>
 				<Input
+					ref={emailInputRef}
 					id="email"
 					label="E-Mail"
 					type="email"
@@ -108,8 +122,8 @@ const Login = (props) => {
 					onChange={emailChangeHandler}
 					onBlur={validateEmailHandler}
 				/>
-
 				<Input
+					ref={passwordInputRef}
 					id="password"
 					label="Password"
 					type="password"
@@ -118,13 +132,8 @@ const Login = (props) => {
 					onChange={passwordChangeHandler}
 					onBlur={validatePasswordHandler}
 				/>
-
 				<div className={classes.actions}>
-					<Button
-						type="submit"
-						className={classes.btn}
-						disabled={!formIsValid}
-					>
+					<Button type="submit" className={classes.btn}>
 						Login
 					</Button>
 				</div>
@@ -134,22 +143,3 @@ const Login = (props) => {
 };
 
 export default Login;
-
-// endNote:
-// when to use useState vs useReducer,
-
-// useState:
-// 	useState is main state management tool.
-// 	Typically, start with useState and often it's all we need.
-// 	It's great for independent pieces of state and data.
-// 	It's great for simple state, we could say.
-// 	It's great if state updates are easy and limited to a few kinds of updates.
-// 	So, if we don't have a lot of different cases that will change a state and especially if we don't have, let's say, an object as a state or anything like that.
-
-// useReducer:
-// 	Now, if we do have an object as a state or a more complex state useReducer might be interesting because, in general, useReducer is great if we need more power and with more power,
-// 	It simply mean that weF can write such a reducer function that can contain more complex state updating logic where we always are guaranteed to work with the latest state snapshot.
-// 	And where we can move that potentially more complex logic out of wer component function body into a separate reducer function.
-// 	So, all the restructure code, that might be interesting.
-// 	We should especially consider useReducer, if we're dealing with related data with state that is made up of related pieces of state.
-// 	So when we, for example, have a scenario, as we had it with our form input state. And, in general, useReducer can be helpful if we have more complex state updates, if we have different cases, different actions that can change a state, cases like this.
