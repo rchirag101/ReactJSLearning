54260898913ed44e14491c5a39608674556efbc3
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-08 16:30:23 +0530

    Refactoring an Input Component

--- react-complete-guide/login-module/src/components/Login/Login.js
+++ react-complete-guide/login-module/src/components/Login/Login.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect, useReducer, useContext } from "react";
 import AuthContext from "../../context/auth-context";
 
 import Card from "../UI/Card/Card";
-import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
+
+import classes from "./Login.module.css";
 
 const emailReducer = (state, action) => {
 	if (action.type === "USER_INPUT") {
@@ -97,34 +99,26 @@ const Login = (props) => {
 	return (
 		<Card className={classes.login}>
 			<form onSubmit={submitHandler}>
-				<div
-					className={`${classes.control} ${
-						emailState.isValid === false ? classes.invalid : ""
-					}`}
-				>
-					<label htmlFor="email">E-Mail</label>
-					<input
-						type="email"
-						id="email"
-						value={emailState.value}
-						onChange={emailChangeHandler}
-						onBlur={validateEmailHandler}
-					/>
-				</div>
-				<div
-					className={`${classes.control} ${
-						passwordState.isValid === false ? classes.invalid : ""
-					}`}
-				>
-					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						id="password"
-						value={passwordState.value}
-						onChange={passwordChangeHandler}
-						onBlur={validatePasswordHandler}
-					/>
-				</div>
+				<Input
+					id="email"
+					label="E-Mail"
+					type="email"
+					isValid={emailIsValid}
+					value={emailState.value}
+					onChange={emailChangeHandler}
+					onBlur={validateEmailHandler}
+				/>
+
+				<Input
+					id="password"
+					label="Password"
+					type="password"
+					isValid={passwordIsValid}
+					value={passwordState.value}
+					onChange={passwordChangeHandler}
+					onBlur={validatePasswordHandler}
+				/>
+
 				<div className={classes.actions}>
 					<Button
 						type="submit"
