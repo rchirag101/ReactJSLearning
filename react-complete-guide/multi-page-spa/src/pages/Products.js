0d56dd1d07de5226d93429626ccc4af3b3968765
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-10 16:43:20 +0530

    modified list of Products

--- react-complete-guide/multi-page-spa/src/pages/Products.js
+++ react-complete-guide/multi-page-spa/src/pages/Products.js
@@ -1,5 +1,16 @@
 function Products() {
-	return <h1>The Products Page</h1>;
+	const listStyle = { listStyleType: "none" };
+
+	return (
+		<section>
+			<h1>The Products Page</h1>
+			<ul style={listStyle}>
+				<li>A Book</li>
+				<li>Laptop</li>
+				<li>Water Bottle</li>
+			</ul>
+		</section>
+	);
 }
 
 export default Products;
