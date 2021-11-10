a7141f8734bd4b32d288ee13a7b17ae8443c796b
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-10 17:31:22 +0530

    added nested route

--- react-complete-guide/multi-page-spa/src/pages/Welcome.js
+++ react-complete-guide/multi-page-spa/src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router";
+
 function Welcome() {
-	return <h1>The Welcome Page</h1>;
+	return (
+		<section>
+			<h1>The Welcome Page</h1>
+			<Route path="/welcome/new-user">
+				<p>Welcome New user</p>
+			</Route>
+		</section>
+	);
 }
 
 export default Welcome;
