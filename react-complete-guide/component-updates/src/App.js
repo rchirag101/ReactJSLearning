40ad2df9b69468cccdaf9aa61d093eb42720a476
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-10 12:02:20 +0530

    added handler to Toggle Paragraph

--- react-complete-guide/component-updates/src/App.js
+++ react-complete-guide/component-updates/src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Button from "./components/UI/Button/Button";
 
 import "./css/App.css";
 
 function App() {
+	const [showParagraph, setShowParagraph] = useState(false);
+
+	const toggleParagraphHandler = () => {
+		setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+	};
+
 	return (
 		<div className="app">
 			<h1>Hi there!</h1>
+			{showParagraph && <p>This is new!</p>}
+			<Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
 		</div>
 	);
 }
