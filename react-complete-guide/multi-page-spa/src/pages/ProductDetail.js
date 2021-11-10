c3b3e70757eca3f499396564b8e74601510c2994
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-10 16:53:00 +0530

    extracted Route Params in ProductDetail page

--- react-complete-guide/multi-page-spa/src/pages/ProductDetail.js
+++ react-complete-guide/multi-page-spa/src/pages/ProductDetail.js
@@ -1,7 +1,16 @@
+import { useParams } from "react-router";
+
 function ProductDetail() {
+	const params = useParams();
+
+	console.log(params);
+	// productId comes from App.js: <Route path="/product-detail/:productId"> <ProductDetail /> </Route>
+	console.log(params.productId);
+
 	return (
 		<section>
 			<h1>The Product Detail</h1>
+			<p>{params.productId}</p>
 		</section>
 	);
 }
