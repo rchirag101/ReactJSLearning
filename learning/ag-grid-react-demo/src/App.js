0f311181e5c6a19e73ff2e39e5695d7629ffe96e
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-10 10:57:22 +0530

    added Grouping (enterprise)

--- learning/ag-grid-react-demo/src/App.js
+++ learning/ag-grid-react-demo/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
-
+import 'ag-grid-enterprise';
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import "./css/App.scss";
