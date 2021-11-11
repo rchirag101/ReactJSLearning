5daa00d07c4bf49615434eb49398ad5d95606d5c
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-11 12:00:35 +0530

    Prevented Possibly Unwanted Route Transitions with the Prompt Component
    Used to prompt the user before navigating away from a page. When your application enters a state that should prevent the user from navigating away (like a form is half-filled out), render a <Prompt>.

--- react-complete-guide/quotes-multi-page-spa/src/components/quotes/QuoteForm.js
+++ react-complete-guide/quotes-multi-page-spa/src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from "react";
+import { Fragment, useRef, useState } from "react";
+import { Prompt } from "react-router";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
@@ -8,6 +9,16 @@ import classes from "./css/QuoteForm.module.css";
 const QuoteForm = (props) => {
 	const authorInputRef = useRef();
 	const textInputRef = useRef();
+
+	const [isEntering, setIsEntering] = useState(false);
+
+	function formFocusHandler() {
+		setIsEntering(true);
+	}
+
+	function finishEnteringHandler() {
+		setIsEntering(false);
+	}
 
 	function submitFormHandler(event) {
 		event.preventDefault();
@@ -21,27 +32,45 @@ const QuoteForm = (props) => {
 	}
 
 	return (
-		<Card>
-			<form className={classes.form} onSubmit={submitFormHandler}>
-				{props.isLoading && (
-					<div className={classes.loading}>
-						<LoadingSpinner />
-					</div>
-				)}
+		<Fragment>
+			<Prompt
+				when={isEntering}
+				message={(location) =>
+					"Are you sure you want to leave? All your entered data will be lost!!!"
+				}
+			/>
+			<Card>
+				<form
+					onFocus={formFocusHandler}
+					className={classes.form}
+					onSubmit={submitFormHandler}
+				>
+					{props.isLoading && (
+						<div className={classes.loading}>
+							<LoadingSpinner />
+						</div>
+					)}
 
-				<div className={classes.control}>
-					<label htmlFor="author">Author</label>
-					<input type="text" id="author" ref={authorInputRef} />
-				</div>
-				<div className={classes.control}>
-					<label htmlFor="text">Text</label>
-					<textarea id="text" rows="5" ref={textInputRef}></textarea>
-				</div>
-				<div className={classes.actions}>
-					<button className="btn">Add Quote</button>
-				</div>
-			</form>
-		</Card>
+					<div className={classes.control}>
+						<label htmlFor="author">Author</label>
+						<input type="text" id="author" ref={authorInputRef} />
+					</div>
+					<div className={classes.control}>
+						<label htmlFor="text">Text</label>
+						<textarea
+							id="text"
+							rows="5"
+							ref={textInputRef}
+						></textarea>
+					</div>
+					<div className={classes.actions}>
+						<button className="btn" onClick={finishEnteringHandler}>
+							Add Quote
+						</button>
+					</div>
+				</form>
+			</Card>
+		</Fragment>
 	);
 };
 
