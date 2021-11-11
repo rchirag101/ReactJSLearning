af70f4219ce0b8069d0432da836708e86a840b27
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-11 11:44:51 +0530

    Implemented useHistory
    it is called Programmatic (Imperative) Navigation

--- react-complete-guide/quotes-multi-page-spa/src/pages/NewQuote.js
+++ react-complete-guide/quotes-multi-page-spa/src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useHistory } from "react-router";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 function NewQuote() {
+	const history = useHistory();
+
 	function addQuoteHandler(quoteData) {
 		console.log(quoteData);
+
+		history.push("/quotes");
 	}
 
 	return <QuoteForm onAddQuote={addQuoteHandler} />;
