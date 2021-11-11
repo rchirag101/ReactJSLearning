6e2f0ef93a99bca388ac262c90855807d3147f1b
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-11 11:10:16 +0530

    displayed DUMMY_QUOTES

--- react-complete-guide/quotes-multi-page-spa/src/pages/AllQuotes.js
+++ react-complete-guide/quotes-multi-page-spa/src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+	{ id: "q1", author: "Chirag", text: "Learning React is fun" },
+	{ id: "q2", author: "Chitti", text: "I'm Chitti" },
+];
+
 function AllQuotes() {
-	return <h1>AllQuotes</h1>;
+	return <QuoteList quotes={DUMMY_QUOTES} />;
 }
 
 export default AllQuotes;
