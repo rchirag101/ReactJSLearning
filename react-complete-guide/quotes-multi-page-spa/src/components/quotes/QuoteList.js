9500ab8bccec0f2936adf9c072e6561a1684b88d
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2021-11-11 12:47:25 +0530

    added sorting functionality based on Query Parameters

--- react-complete-guide/quotes-multi-page-spa/src/components/quotes/QuoteList.js
+++ react-complete-guide/quotes-multi-page-spa/src/components/quotes/QuoteList.js
@@ -5,6 +5,16 @@ import QuoteItem from "./QuoteItem";
 
 import classes from "./css/QuoteList.module.css";
 
+function sortQuotes(quotes, ascending) {
+	return quotes.sort((quoteA, quoteB) => {
+		if (ascending) {
+			return quoteA.id > quoteB.id ? 1 : -1;
+		} else {
+			return quoteA.id < quoteB.id ? 1 : -1;
+		}
+	});
+}
+
 const QuoteList = (props) => {
 	// useHistory gives us access to the history object, an object that allows us to change and manage the URL,
 	// useLocation gives us access to a location object which has information about the currently loaded page, about the currently loaded URL.
@@ -15,6 +25,8 @@ const QuoteList = (props) => {
 	const queryParams = new URLSearchParams(location.search);
 	const isSortingAscending = queryParams.get("sort") === "asc";
 
+	const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
 	function changeSortingHandler() {
 		history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
 	}
@@ -23,12 +35,12 @@ const QuoteList = (props) => {
 		<Fragment>
 			<div className={classes.sorting}>
 				<button onClick={changeSortingHandler}>
-					Sort {isSortingAscending ? "Descending" : "Ascending"}
+					Sort {isSortingAscending ? "Descending ↓" : "Ascending ↑"}
 				</button>
 			</div>
 
 			<ul className={classes.list}>
-				{props.quotes.map((quote) => (
+				{sortedQuotes.map((quote) => (
 					<QuoteItem
 						key={quote.id}
 						id={quote.id}
